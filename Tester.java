9ad8c8b807661c1a49d7e8a8964c0b6b2b2a14de
FanciestW <wlin26@yahoo.com>
2014-12-12 00:43:19 -0500

    Added name function

--- Tester.java
+++ Tester.java
@@ -4,5 +4,8 @@ import java.util.*;
 public class Tester{
 	public static void main(String[]args){
 		System.out.println("Tester");
+		Scanner scName = new Scanner(System.in);
+		String name = scName.next();
+		System.out.println("Your name is: "+name);
 	}
 }
