db24face4f480cb38feb08d3c049d56d09ea211c
David Friedenberg <friedenberg12@aol.com>
2014-12-30 21:16:48 -0500

    Found this updated Tetris game and decided to add it Might update more in the future

---
+++ tetris.lua
@@ -0,0 +1,51 @@
+scriptId = 'com.thalmic.examples.outputeverything'
+scriptTitle = "Output Everything"
+scriptDetailsUrl = "" -- We don't have this until it's submitted to the Myo Market
+
+function onPoseEdge(pose, edge)
+
+	myo.unlock("hold")
+    myo.debug("onPose: " .. pose.. " edge:"..edge)
+    if(pose == "waveOut")then
+    	if(edge == "on")then
+	    	myo.keyboard("d","press")
+	    	myo.keyboard("return","press")
+    	elseif(edge == "off")then
+    	end
+
+    elseif(pose == "waveIn")then
+    	if(edge == "on")then
+	    	myo.keyboard("a","press")
+	    	myo.keyboard("return","press")
+    	elseif(edge == "off")then
+    	end
+    elseif(myo.getRoll()>2.5)then
+    	if(edge == "on")then
+	    	myo.keyboard("w","press")
+	    	myo.keyboard("return","press")
+    	elseif(edge == "off")then
+    	end
+    elseif(myo.getRoll()<-2.5)then
+    	if(edge == "on")then
+	    	myo.keyboard("s","press")
+	    	myo.keyboard("return","press")
+    	elseif(edge == "off")then
+    	end
+    end
+end
+
+function onPeriodic()
+end
+
+function onForegroundWindowChange(app, title)
+    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+function activeAppName()
+    return "Output Everything"
+end
+
+function onActiveChange(isActive)
+    myo.debug("onActiveChange")
+end
